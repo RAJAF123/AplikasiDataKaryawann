84d30e8101d80872990d0cf4b91c97ac0b1607fa
RAJAF123 <92904410+RAJAF123@users.noreply.github.com>
2022-11-05 10:27:26 +0700

    Add files via upload

---
+++ navigasi/Karyawan.js
@@ -0,0 +1,10 @@
+const karyawan = [
+    {id:'1',nama:'Dian Wahyuninsih', jabatan:'Ses.Prodi TI',
+    email:'dian.wahyuningsih@stimata.ac.id',telp:'085755482039',
+    gaji:'1.000.000',foto:'https://placeimg.com/640/480/people'},
+    
+    {id:'2',nama:'Fajar', jabatan:'ketua SEC',
+    email:'fajarkurniawan129a@gmail.com',telp:'081359135500',
+    gaji:'0',foto:'https://placeimg.com/640/480/people'}
+];
+export default karyawan;
